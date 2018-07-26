4ea8cf34d0c6a0274fe37332cca0a3a851d6379d
cristianpb <felipebrokate@gmail.com>
2018-07-27 00:54:16 +0200

    Add youtube videos Add dropdown menu for communications Remove controls from leafletmaps Include medium account

--- assets/js/map_leaflet.js
+++ assets/js/map_leaflet.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([25, -30], 2);
+var mymap = L.map('mapid',{zoomControl: false}).setView([25, -30], 2);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
