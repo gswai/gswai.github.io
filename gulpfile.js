ee0f2978f3609614fc39bca5ca7632ef40040e09
cristianpb <felipebrokate@gmail.com>
2018-08-16 09:15:36 +0200

    Inline navburger click Use leafletjs at the end of html

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,12 @@ var gulp = require('gulp'),
   insert = require('gulp-insert'),
   gp_uglify = require('gulp-uglify');
 
-//gulp.task('main1', function(){
-//  return gulp.src(['./assets/js/map_leaflet_landing.js'])
-//    .pipe(gp_concat('concat.js'))
-//    .pipe(gulp.dest('./_includes/js/'))
-//    .pipe(gp_rename({ extname: '.min.js' }))
-//    .pipe(gp_uglify())
-//    .pipe(gulp.dest('./_includes/js/'));
-//});
-//
-//gulp.task('default', ['main1'], function(){});
+gulp.task('navburger', function(){
+  return gulp.src(['./assets/js/navburger_click.js'])
+    .pipe(gp_rename({ extname: '.min.js' }))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./_includes/'));
+});
 
 gulp.task('map_leaflet_landing', function () {
   return gulp.src('./assets/js/map_leaflet_landing.js')
@@ -29,4 +25,4 @@ gulp.task('map_leaflet', function () {
     .pipe(gulp.dest('./_includes/'));
 });
 
-gulp.task('default', ['map_leaflet_landing', 'map_leaflet'], function(){});
+gulp.task('default', ['navburger', 'map_leaflet_landing', 'map_leaflet'], function(){});
