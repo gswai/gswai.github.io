bc9faea18cff082b23ffc8e578cff36d445276a3
cristianpb <felipebrokate@gmail.com>
2018-08-16 18:21:21 +0200

    Inline defer_css_laoding

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
   gp_concat = require('gulp-concat'),
   gp_rename = require('gulp-rename'),
-  insert = require('gulp-insert'),
   gp_uglify = require('gulp-uglify');
 
 gulp.task('navburger', function(){
@@ -25,4 +24,11 @@ gulp.task('map_leaflet', function () {
     .pipe(gulp.dest('./_includes/'));
 });
 
-gulp.task('default', ['navburger', 'map_leaflet_landing', 'map_leaflet'], function(){});
+gulp.task('defer_css_loading', function () {
+  return gulp.src('./assets/js/defer_css_loading.js')
+    .pipe(gp_rename({ extname: '.min.js' }))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./_includes/'));
+});
+
+gulp.task('default', ['navburger', 'map_leaflet_landing', 'map_leaflet', 'defer_css_loading'], function(){});
