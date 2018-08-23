5501b5d7b64024679809065b2ed7dd31def2b611
cristianpb <felipebrokate@gmail.com>
2018-08-23 23:32:06 +0200

    amp page

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,13 @@ var gulp = require('gulp'),
   gp_rename = require('gulp-rename'),
   image = require('gulp-image'),
   gp_uglify = require('gulp-uglify');
+var replace = require('gulp-replace');
+var sass = require('gulp-sass');
+var purify = require('gulp-purifycss');
+let cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
-gulp.task('navburger', function(){
-  return gulp.src(['./assets/js/navburger_click.js'])
-    .pipe(gp_rename({ extname: '.min.js' }))
-    .pipe(gp_uglify())
-    .pipe(gulp.dest('./_includes/'));
-});
-
-gulp.task('map_leaflet_landing', function () {
-  return gulp.src('./assets/js/map_leaflet_landing.js')
-    .pipe(gp_rename({ extname: '.min.js' }))
-    .pipe(gp_uglify())
-    .pipe(gulp.dest('./_includes/'));
-});
-
-gulp.task('map_leaflet', function () {
-  return gulp.src('./assets/js/map_leaflet.js')
-    .pipe(gp_rename({ extname: '.min.js' }))
-    .pipe(gp_uglify())
-    .pipe(gulp.dest('./_includes/'));
-});
-
-gulp.task('defer_css_loading', function () {
-  return gulp.src('./assets/js/defer_css_loading.js')
-    .pipe(gp_rename({ extname: '.min.js' }))
-    .pipe(gp_uglify())
-    .pipe(gulp.dest('./_includes/'));
-});
+var site = 'https://d40e46fc.ngrok.io';
 
 gulp.task('image', function () {
   gulp.src('./assets/images/*')
@@ -49,4 +28,25 @@ gulp.task('image', function () {
     .pipe(gulp.dest('./assets/images/'));
 });
 
-gulp.task('default', ['navburger', 'map_leaflet_landing', 'map_leaflet', 'defer_css_loading'], function(){});
+gulp.task('sass', function () {
+  return gulp.src('./_sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./_sass/'));
+});
+
+// purify removes unused CSS classes
+gulp.task('purify', ['sass'],  function() {
+  return gulp.src('./_sass/main.css')
+    .pipe(purify(['./_layouts/**.html', './_pages/**.html'], {info: true}))
+    .pipe(replace(/!important/gm, ''))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({ suffix: '-min' }))
+    .pipe(gulp.dest('./_includes'));
+});
+
+gulp.task("watch", ["purify"], function() {
+  gulp.watch(["./_sass/main.scss", "./_sass/_variables.scss"], ["sass", "purify"]);
+  gulp.watch(["./_pages/**", "./_layouts/**", "./_includes/**.html", "./blog/*"], ["purify"]);
+})
+
+gulp.task('default', ['sass', 'purify'], function(){});
