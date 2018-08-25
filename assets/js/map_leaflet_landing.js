1b73c326cfbd8ca734cdfe4d9afbb2d4dbc44f56
cristianpb <felipebrokate@gmail.com>
2018-08-25 21:36:19 +0200

    Add Madagascar, Jeddah and Doula -> Yaoundé

--- assets/js/map_leaflet_landing.js
+++ assets/js/map_leaflet_landing.js
@@ -19,14 +19,16 @@ var LeafIcon = L.Icon.extend({
 
 var gswaiIcon = new LeafIcon({iconUrl: '/assets/flags/gswai.svg'})
 
-L.marker([43.6108, 3.8767], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br /> Paris, France');
-L.marker([7.369721999999999, 12.354722000000038], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Douala, Cameroon').openPopup();
-L.marker([-9.189967, -75.015152], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Lima, Peru');
-L.marker([22.28552, 114.15769], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Hong Kong, China');
-L.marker([19.4326077, -99.13320799999997], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Mexico city, Mexico');
-L.marker([33.886917, 9.537499000000025], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Carthage, Tunisia');
-L.marker([41.0082376, 28.9783589], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Istanbul, Turkey');
-L.marker([-16.290154, -63.58865300000002], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />La Paz, Bolivia');
-L.marker([50.85045, 4.34878], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Bruxelles, Belgium');
-L.marker([25.105497, 121.597366], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Taipei, Taiwan');
-L.marker([-33.8830555556, 151.21666666725], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI</b><br />Sydney, Australia');
+L.marker([43.6108, 3.8767], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br /> Paris, France');
+L.marker([7.369721999999999, 12.354722000000038], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Yaoundé, Cameroon');
+L.marker([21.54238, 39.19797], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Jeddah, Saudi Arabia');
+L.marker([-18.9333, 47.51674], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Antananarivo, Madagascar');
+L.marker([45.5016889, -73.56725599999999], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br /> Montreal, Canada').openPopup();
+L.marker([-9.189967, -75.015152], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Lima, Peru');
+L.marker([22.28552, 114.15769], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Hong Kong, China');
+L.marker([33.886917, 9.537499000000025], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Carthage, Tunisia');
+L.marker([41.0082376, 28.9783589], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Istanbul, Turkey');
+L.marker([-16.290154, -63.58865300000002], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />La Paz, Bolivia');
+L.marker([50.85045, 4.34878], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Bruxelles, Belgium');
+L.marker([25.105497, 121.597366], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Taipei, Taiwan');
+L.marker([-33.8830555556, 151.21666666725], {icon: gswaiIcon}).addTo(mymap).bindPopup('<b>Startup Weekend AI 2018</b><br />Sydney, Australia');
