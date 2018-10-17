871291b24274b1c3252062320100e7a60bb78c5f
cristianpb <felipebrokate@gmail.com>
2018-10-18 00:10:55 +0200

    Add svg anim for get involved Add carousel for medium articles

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,45 @@ var gulp = require('gulp'),
   image = require('gulp-image'),
   gp_uglify = require('gulp-uglify');
 var replace = require('gulp-replace');
+var responsive = require('gulp-responsive');
 var sass = require('gulp-sass');
 var purify = require('gulp-purifycss');
 let cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
 var site = 'https://d40e46fc.ngrok.io';
+
+gulp.task('responsive', function () {
+  return gulp.src('./assets/images/posts/post_*')
+    .pipe(responsive({
+      //'*/main.jpg': {
+      //  // Resize all JPG images to 200 pixels wide
+      //  width: 200,
+      //},
+      //'*/main.png': {
+      //  // Resize all PNG images to 50% of original pixels wide
+      //  width: '50%',
+      //},
+      // Resize all images to 100 pixels wide and add suffix -thumbnail
+      '*': {
+        width: 300,
+        height: 200,
+        rename: { suffix: '-crop' },
+      },
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 70,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Zlib compression level of PNG output format
+      compressionLevel: 6,
+      // Strip all metadata
+      withMetadata: false,
+      crop: 'entropy'
+    }))
+    .pipe(gulp.dest('./assets/images/posts'));
+});
 
 gulp.task('image', function () {
   gulp.src('./assets/images/*')
